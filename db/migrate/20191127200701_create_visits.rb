870116aa4c06ea80319a1aaa0cdd696a57479039
joshIsCoding <yes_you_do@hotmail.com>
2019-11-27 20:14:34 +0000

    Generate and run visits table migration

---
+++ db/migrate/20191127200701_create_visits.rb
@@ -0,0 +1,10 @@
+class CreateVisits < ActiveRecord::Migration[6.0]
+  def change
+    create_table :visits do |t|
+      t.integer :visitor_id, null: false
+      t.integer :shortened_url_id, null: false
+
+      t.timestamps
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_27_170043) do
+ActiveRecord::Schema.define(version: 2019_11_27_200701) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -30,6 +30,13 @@ ActiveRecord::Schema.define(version: 2019_11_27_170043) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["email"], name: "index_users_on_email", unique: true
+  end
+
+  create_table "visits", force: :cascade do |t|
+    t.integer "visitor_id", null: false
+    t.integer "shortened_url_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
 end
