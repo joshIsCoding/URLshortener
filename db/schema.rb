73aa658d2f23b90f925341cdcdf70514def15788
joshIsCoding <yes_you_do@hotmail.com>
2019-11-27 17:28:01 +0000

    Create shortened_urls table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_27_123342) do
+ActiveRecord::Schema.define(version: 2019_11_27_170043) do
+
+  create_table "shortened_urls", force: :cascade do |t|
+    t.string "long_url", null: false
+    t.string "short_url"
+    t.integer "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["short_url"], name: "index_shortened_urls_on_short_url", unique: true
+    t.index ["user_id"], name: "index_shortened_urls_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", null: false
