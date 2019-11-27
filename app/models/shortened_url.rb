4debf4f0ee88bcd9e17d6e7c02e3efad5c9e3597
joshIsCoding <yes_you_do@hotmail.com>
2019-11-27 20:02:47 +0000

    Add factory method ShortenedUrl::generate_short_url! to create, save and return a ShortenedUrl object

--- app/models/shortened_url.rb
+++ app/models/shortened_url.rb
@@ -13,4 +13,8 @@ class ShortenedUrl < ApplicationRecord
       new_code = SecureRandom.urlsafe_base64 until new_code && !exists?(short_url: new_code)
       new_code
    end
+
+   def self.generate_short_url!(user, long_url)
+      create!(:user_id => user.id, :long_url => long_url, :short_url => ShortenedUrl.random_code)
+   end
 end
