89290699c7874412a4cd6efed58eb41d5fd3e571
joshIsCoding <yes_you_do@hotmail.com>
2019-11-30 23:46:57 +0000

    Generate and run migration to add a premium column to users table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_30_181957) do
+ActiveRecord::Schema.define(version: 2019_11_30_234008) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -39,6 +39,7 @@ ActiveRecord::Schema.define(version: 2019_11_30_181957) do
     t.string "email", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.boolean "premium", default: false
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
