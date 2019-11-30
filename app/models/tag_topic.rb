96005933d57bc448279d5139e18b9e23313fd7c7
joshIsCoding <yes_you_do@hotmail.com>
2019-11-30 22:32:05 +0000

    Write #popular_links query to return 5 most visited links under a given topic tag

--- app/models/tag_topic.rb
+++ app/models/tag_topic.rb
@@ -7,4 +7,15 @@ class TagTopic < ApplicationRecord
       foreign_key: :tag_topic_id,
       primary_key: :id
    )
+
+   has_many(
+      :short_urls,
+      -> { distinct },
+      through: :taggings,
+      source: :short_url
+   )
+
+   def popular_links
+      short_urls.joins(:visits).group(:short_url, :long_url).order("COUNT(visits.id) DESC").select(:long_url, :short_url, "COUNT(visits.id) AS num_visits").limit(5)
+   end
 end
